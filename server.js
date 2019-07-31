3518584dc4cc7cae50b22e9683b224441aae5bb8
Taras <taras.prezlyata.sign.in@gmail.com>
2019-07-31 22:47:02 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Task = require("./models/task");
+const app = express();
+
+app.use(bodyParser.json(), cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = 5000;
+const SERVER = "127.0.0.1:27017";
+const DB = "tasks-app";
+const MONGODB_URI = `mongodb://${SERVER}/${DB}`;
+const connection = mongoose.connection;
+const option = {
+  socketTimeoutMS: 30000,
+  keepAlive: true,
+  reconnectTries: 30000
+};
+
+mongoose.Promise = Promise;
+mongoose.connect(
+  MONGODB_URI,
+  option
+);
+
+connection.on("connected", () => console.log("Connected to database"));
+connection.on("error", err => console.log("Connection failed with - ", err));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+app.get("/tasks", (req, res, next) => {
+  Task.find((err, tasks) => {
+    if (err) return next(err);
+    res.json(tasks);
+  });
+});
+
+app.post("/tasks", (req, res, next) => {
+  Task.create(req.body, (err, task) => {
+    if (err) return next(err);
+    res.json(task);
+  });
+});
+
+app.delete("/tasks:id", function(req, res, next) {
+  Task.findByIdAndRemove(req.params.id.substr(1), req.body, (err, post) => {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import './App.scss';
+import axios from 'axios';
+import picture from './img/picture.jpg';
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			tasks: null,
+			loading: true,
+			taskTitle: ''
+		};
+	}
+
+	componentDidMount() {
+		this.getTasks();
+	}
+
+	getTasks = () => {
+		axios
+			.get('http://localhost:5000/tasks')
+			.then((response) => {
+				this.setState({
+					tasks: response.data,
+					loading: false
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	addTask = () => {
+		const { taskTitle } = this.state;
+
+		if (taskTitle.length) {
+			axios
+				.post('http://localhost:5000/tasks', { title: taskTitle })
+				.then((res) => {
+					console.log(res);
+					this.getTasks();
+					this.setState({
+						taskTitle: ''
+					});
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	};
+
+	deleteTask = (e) => {
+		let id = e.target.parentNode.getAttribute('data-id');
+
+		axios
+			.delete(`http://localhost:5000/tasks:${id}`)
+			.then((res) => {
+				console.log(res);
+				this.getTasks();
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	onTodoChange(value) {
+		this.setState({
+			taskTitle: value
+		});
+	}
+
+	renderList = () => {
+		if (!this.state.loading) {
+			return (
+				<div className="list">
+					{this.state.tasks.map((task, index) => {
+						return (
+							<div className="list-task" key={index} data-id={task._id}>
+								<img src={picture} alt="picture" />
+								<p className="list-group-item list-group-item-action" key={index}>
+									{task.title}
+								</p>
+								<button type="button" className="btn btn-danger" onClick={(e) => this.deleteTask(e)}>
+									Delete
+								</button>
+							</div>
+						);
+					})}
+				</div>
+			);
+		}
+	};
+
+	render() {
+		console.log(this.state);
+		return (
+			<div className="App">
+				<div className="app-form">
+					<input
+						type="text"
+						className="form-control"
+						placeholder="name"
+						onChange={(e) => this.onTodoChange(e.target.value)}
+						value={this.state.taskTitle}
+					/>
+					<button type="submit" className="btn btn-primary" onClick={this.addTask}>
+						Create
+					</button>
+				</div>
+				{this.renderList()}
+			</div>
+		);
+	}
+}
+
+export default App;
